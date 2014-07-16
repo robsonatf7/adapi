048342dd32e95a490da23d2b45478615d001ea0c
robsonatf7 <robson@f7technology.com>
2014-07-16 20:00:57 -0300

    working seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create(id: 1, name: "Celulares")
+Category.create(id: 2, name: "Carros")
+Category.create(id: 3, name: "Para Casa")
+Category.create(id: 4, name: "Moda e Beleza")
+
+Ad.create(id: 1, title: "iphone", price: 8, location: "Sao Paulo", 
+		  description: "nonononononononononononononononononononono",
+		  category_id: 1, user_email: "robson@robson.com")
+Ad.create(id: 2, title: "iphone", price: 8, location: "Sao Paulo", 
+		  description: "nonononononononononononononononononononono",
+		  category_id: 1, user_email: "robson@robson.com")
+Ad.create(id: 3, title: "iphone", price: 8, location: "Sao Paulo", 
+		  description: "nonononononononononononononononononononono",
+		  category_id: 2, user_email: "robson@robson.com")
+Ad.create(id: 4, title: "iphone", price: 8, location: "Sao Paulo", 
+		  description: "nonononononononononononononononononononono",
+		  category_id: 2, user_email: "robson@robson.com")
+Ad.create(id: 5, title: "iphone", price: 8, location: "Sao Paulo", 
+		  description: "nonononononononononononononononononononono",
+		  category_id: 3, user_email: "robson@robson.com")
+Ad.create(id: 6, title: "iphone", price: 8, location: "Sao Paulo", 
+		  description: "nonononononononononononononononononononono",
+		  category_id: 3, user_email: "robson@robson.com")
+Ad.create(id: 7, title: "iphone", price: 8, location: "Sao Paulo", 
+		  description: "nonononononononononononononononononononono",
+		  category_id: 4, user_email: "robson@robson.com")
+Ad.create(id: 8, title: "iphone", price: 8, location: "Sao Paulo", 
+		  description: "nonononononononononononononononononononono",
+		  category_id: 4, user_email: "robson@robson.com")
