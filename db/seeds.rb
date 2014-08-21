9edf7ca5f14a909f3b5f9a9f8c388b3b36b04e38
Marcelo Ribeiro <ribeirodesenv@gmail.com>
2014-08-21 08:58:55 -0700

    Ignoring images folder; moving to mysql instead of sqlite; making sure Ads have differente titles; Create on DB Seed should raise exception if errors occur

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Category.create( name: "Celulares", image: File.new("#{Rails.root}/app/assets/images/icon_phone.png"))
-Category.create( name: "Tablets", image: File.new("#{Rails.root}/app/assets/images/icon_tablet.png"))
-Category.create( name: "Eletrônicos", image: File.new("#{Rails.root}/app/assets/images/icon_tv.png"))
-Category.create( name: "Video-games", image: File.new("#{Rails.root}/app/assets/images/icon_joystick.png"))
-Category.create( name: "Informática", image: File.new("#{Rails.root}/app/assets/images/icon_pc.png"))
-Category.create( name: "Esportes", image: File.new("#{Rails.root}/app/assets/images/icon_bike.png"))
-Category.create( name: "Arte e Lazer", image: File.new("#{Rails.root}/app/assets/images/icon_guitar.png"))
-Category.create( name: "Veículos", image: File.new("#{Rails.root}/app/assets/images/icon_car.png"))
-Category.create( name: "Para Casa", image: File.new("#{Rails.root}/app/assets/images/icon_house.png"))
-Category.create( name: "Moda e Beleza", image: File.new("#{Rails.root}/app/assets/images/icon_shirt.png"))
-Category.create( name: "Bebê e Criança", image: File.new("#{Rails.root}/app/assets/images/icon_baby.png"))
-Category.create( name: "Outros", image: File.new("#{Rails.root}/app/assets/images/icon_other.png"))
+Category.create!( name: "Celulares", image: File.new("#{Rails.root}/app/assets/images/icon_phone.png"))
+Category.create!( name: "Tablets", image: File.new("#{Rails.root}/app/assets/images/icon_tablet.png"))
+Category.create!( name: "Eletrônicos", image: File.new("#{Rails.root}/app/assets/images/icon_tv.png"))
+Category.create!( name: "Video-games", image: File.new("#{Rails.root}/app/assets/images/icon_joystick.png"))
+Category.create!( name: "Informática", image: File.new("#{Rails.root}/app/assets/images/icon_pc.png"))
+Category.create!( name: "Esportes", image: File.new("#{Rails.root}/app/assets/images/icon_bike.png"))
+Category.create!( name: "Arte e Lazer", image: File.new("#{Rails.root}/app/assets/images/icon_guitar.png"))
+Category.create!( name: "Veículos", image: File.new("#{Rails.root}/app/assets/images/icon_car.png"))
+Category.create!( name: "Para Casa", image: File.new("#{Rails.root}/app/assets/images/icon_house.png"))
+Category.create!( name: "Moda e Beleza", image: File.new("#{Rails.root}/app/assets/images/icon_shirt.png"))
+Category.create!( name: "Bebê e Criança", image: File.new("#{Rails.root}/app/assets/images/icon_baby.png"))
+Category.create!( name: "Outros", image: File.new("#{Rails.root}/app/assets/images/icon_other.png"))
 
 12.times do
 	for i in (1..12) do
-		Ad.create( title: "Brand new iPhone 5", price: 8, location: "Sao Paulo", 
-		  	   	   description: "nonononononononononononononononononononono",
+		Ad.create!( title: "Brand new iPhone 5 Index #{i}", price: 8, location: "Sao Paulo", 
+		  	   	   description: "Description for iPhone: #{i}",
 		  	       category_id: i, user_email: "robsonsky@yahoo.com.br",
 		  	       image: File.new("#{Rails.root}/app/assets/images/iphone.jpg"))
 	end
